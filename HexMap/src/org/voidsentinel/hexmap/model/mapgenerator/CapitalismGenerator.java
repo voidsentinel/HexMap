d9b54c889ebcb67153da0b3aa11600744508d1de
voidsentinel <mgpatry@gmail.com>
2019-11-28 12:20:49 +0100

    Torus operation redonne (seamless operation)

--- HexMap/src/org/voidsentinel/hexmap/model/mapgenerator/CapitalismGenerator.java
+++ HexMap/src/org/voidsentinel/hexmap/model/mapgenerator/CapitalismGenerator.java
@@ -3,9 +3,11 @@ package org.voidsentinel.hexmap.model.mapgenerator;
 import org.voidsentinel.hexmap.model.HexMap;
 import org.voidsentinel.hexmap.model.mapgenerator.heightmap.generation.CellularGeneration;
 import org.voidsentinel.hexmap.model.mapgenerator.heightmap.generation.FaultCirclesGeneration;
+import org.voidsentinel.hexmap.model.mapgenerator.heightmap.generation.FaultLinesGeneration;
 import org.voidsentinel.hexmap.model.mapgenerator.heightmap.generation.FlatGeneration;
 import org.voidsentinel.hexmap.model.mapgenerator.heightmap.operation.HexBlurOperation;
 import org.voidsentinel.hexmap.model.mapgenerator.heightmap.operation.TorusOperation;
+import org.voidsentinel.hexmap.model.mapgenerator.heightmap.operation.TorusOperation.Direction;
 import org.voidsentinel.hexmap.model.mapgenerator.operations.ElevationMapOperation;
 import org.voidsentinel.hexmap.model.mapgenerator.operations.FertilityMapOperation;
 import org.voidsentinel.hexmap.model.mapgenerator.operations.HeightMapOperation;
@@ -20,28 +22,28 @@ public class CapitalismGenerator extends MapGenerator {
 		LOG.info("Generating map " + map.WIDTH + "/" + map.HEIGHT + " using " + this.getClass().getSimpleName());
 
 		HeightMapOperation heightmap = new HeightMapOperation();
-		
+
 		// define the height
 		heightmap.addGenerator(new FlatGeneration(0f));
 
 //		heightmap.addGenerator(new IslandGeneration(1d), 0.60f);
 //		heightmap.addGenerator(new DiamondSquareGeneration(), 0.40f);
 		heightmap.addGenerator(new CellularGeneration(1f), 0.20f);
-      heightmap.addGenerator(new FaultCirclesGeneration(2000), 0.4f);
-		
+		heightmap.addGenerator(new FaultLinesGeneration(2000), 0.4f);
+		heightmap.addGenerator(new FaultCirclesGeneration(1000), 0.4f);
+
 		heightmap.addOperation(new HexBlurOperation(3, 3));
 //		heightmap.addGenerator(new CellularGeneration(1f), 0.30f);
 //		heightmap.addGenerator(new FaultCirclesGeneration(2000), 0.70f);
-      heightmap.addOperation(new TorusOperation());
+		heightmap.addOperation(new TorusOperation(Direction.both));
 //		heightmap.addOperation(new HexBlurOperation(3, 3));
 //		heightmap.addOperation(new IslandOperation());
 //		heightmap.addOperation(new PowerOperation(4d, 1.0d));
 		heightmap.filter(map);
-		
+
 		// water level
 		new WaterLevelOperation(0.4f).filter(map);
 
-	
 		new ElevationMapOperation(3, 20).filter(map);
 		new WaterPropagationOperation().filter(map);
 		new TemperatureMapOperation().filter(map);
@@ -50,6 +52,6 @@ public class CapitalismGenerator extends MapGenerator {
 		new FertilityMapOperation().filter(map);
 //		new PathMapOperation().filter(map);
 //		new CityMapOperation().filter(map);
-		
+
 	}
 }
--- HexMap/src/org/voidsentinel/hexmap/model/mapgenerator/heightmap/operation/TorusOperation.java
+++ HexMap/src/org/voidsentinel/hexmap/model/mapgenerator/heightmap/operation/TorusOperation.java
@@ -4,13 +4,29 @@
 package org.voidsentinel.hexmap.model.mapgenerator.heightmap.operation;
 
 /**
- * @author guipatry
+ * This operation take a map and modify height so that the map is seamless :
+ * left border can be joigned to right border (for horizontal seamless)
+ * 
+ * @author VoidSentinel
  *
  */
 public class TorusOperation extends AbstractTerrainOperation {
 
+	// direction of operation
+	public enum Direction {
+		vertical, horizontal, both
+	};
 
-	public TorusOperation() {
+	private Direction direction = Direction.both;
+
+	/**
+	 * 
+	 * @param direction the direction of the transformation. If horizontal, only
+	 *                  left and right will be matched. if vertical, top and bottom
+	 *                  will be matched. if both then both operation will be done
+	 */
+	public TorusOperation(Direction direction) {
+		this.direction = direction;
 	}
 
 	public void filter(float[][] height) {
@@ -24,17 +40,29 @@ public class TorusOperation extends AbstractTerrainOperation {
 		for (int y = 0; y < height.length; y++) {
 			for (int x = 0; x < height[0].length; x++) {
 				double cx = Math.abs(x - centerX) / centerX; // range 0-1 (going from 1 to 0 to 1)
-				cx = Math.pow(cx, 4d) * 0.7d;
+				if (direction != Direction.vertical) {
+					cx = Math.pow(cx, 2d);
+				} else {
+					cx = 0d;
+				}
 
 				double cy = Math.abs(y - centerY) / centerY;
-				cy = Math.pow(cy, 4d) * 0.7d;
+				if (direction != Direction.horizontal) {
+					cy = Math.pow(cy, 2d);
+				} else {
+					cy = 0d;
+				}
 
-				float v1 = height[height.length - 1 - y][x] * (float) cy / 2f;
-				float v2 = height[y][height[0].length - 1 - x] * (float) cx / 2f;
-				float v3 = height[height.length - 1 - y][height[0].length - 1 - x] * (float) ((cx + cy) / 4d);
+				double cxy = (cx + cy) / 2d;
+				if (direction != Direction.both) {
+					cxy = 0d;
+				}
+				double total = 1f + cx + cy + cxy;
 
-				float cfinal = 1f - (float) (cx / 2d + cy / 2d + ((cx + cy) / 4d));
-				float v0 = height[y][x] * cfinal;
+				float v0 = height[y][x] * (float) (1d / total);
+				float v1 = height[height.length - 1 - y][x] * (float) (cy / total);
+				float v2 = height[y][height[0].length - 1 - x] * (float) (cx / total);
+				float v3 = height[height.length - 1 - y][height[0].length - 1 - x] * (float) (cxy / total);
 
 				copy[y][x] = v0 + v1 + v2 + v3;
 			}
